dc92ef2e95e921a304282f55e334c29e3dd471b6
miaojiuchen <miaojiuchen@outlook.com>
2019-11-26 17:21:54 +0800

    增加启动参数

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,22 @@
 
 import { ClientType, ClientFactory, TuClient, /* WebClient,*/ IClient } from "./client";
 import * as _ from "lodash";
-import * as os from "os";
 
 const factory = ClientFactory.instance;
 
-const tu_clients: Array<IClient> = _.range(1666).map(i => {
-    return factory.create(ClientType.Tu);
-});
+process.on("message", msg => {
+    const { perCpucoreRequests, wsAddress, processer_count, targetTps } = msg;
+    const connectionDelay = 1000 / (targetTps / processer_count);
+    console.log("worker delay during connections:", connectionDelay, "ms");
+    const tu_clients: Array<IClient> = _.range(perCpucoreRequests).map(() => factory.create(ClientType.Tu));
+    emitConnections(tu_clients, wsAddress, connectionDelay);
+})
 
-const processer_count = os.cpus().length;
-const targetTPS = 500;
-const connectionDelay = 1000 / (targetTPS / processer_count);
-console.log("worker delay during connections:", connectionDelay, "ms");
-
-async function tps(arr: Array<IClient>) {
+async function emitConnections(arr: Array<IClient>, wsAddress: string, connectionDelay: number) {
     for (let i = 0; i < arr.length; ++i) {
         const client = arr[i];
-        arr[i].connect("ws://localhost:7788/invoker/client/wstty?id=" + client.id)
+        arr[i].connect(`${wsAddress}?id=${client.id}`);
+
         await new Promise(resolve => setTimeout(resolve, connectionDelay));
     }
 }
-
-tps(tu_clients);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import * as cp from "child_process";
 import * as os from "os";
+import * as path from "path";
+import * as yargsParser from "yargs-parser";
+
+var argv = yargsParser(process.argv.slice(2), {
+    default: {
+        perCpucoreRequests: 2000,
+        wsAddress: "ws://192.168.1.149:7788/invoker/client/wstty",
+        targetTps: 500
+    }
+});
+
+const { perCpucoreRequests, wsAddress, targetTps } = argv;
+console.log("perCpucoreRequests:", perCpucoreRequests)
+console.log("wsAddress:", wsAddress);
+console.log("targetTps:", targetTps);
 
 const processer_count = os.cpus().length;
 
 for (let i = 0; i < processer_count; ++i) {
-    cp.fork("dist/app.js");
+    const child = cp.fork(path.join(__dirname, 'app.js'));
+    child.send({ perCpucoreRequests, wsAddress, processer_count, targetTps })
 }
